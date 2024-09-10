19ba8550349458b9a9b2ca18e087dfe7ff0d4495
DarshanTank18 <tankdarshan18@gmail.com>
2024-09-10 16:06:55 +0530

    first commit

--- Assignment/Module - 4/Pro - 2.py
+++ Assignment/Module - 4/Pro - 2.py
@@ -1,6 +1,6 @@
 # Write a Python program to read an entire text file.
 
-fl=open('Data.txt','a')
-fl=open('Data.txt','r')
-read=fl.read()
+fl = open('Data.txt','a')
+fl = open('Data.txt','r')
+read = fl.read()
 print(read)
